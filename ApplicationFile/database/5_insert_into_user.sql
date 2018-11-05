fc560c49a4e1c6f2e165b12e931fa8780df688d0
bankairo <bankairo@gmail.com>
2018-11-05 10:12:06 +0700

    create template login and init resource bundle

---
+++ ApplicationFile/database/5_insert_into_user.sql
@@ -0,0 +1,3 @@
+use toeiconline
+INSERT INTO user(name, password, fullname, createddate, roleid) VALUES ('admin', '123456', 'admin', CURRENT_TIMESTAMP, 1);
+INSERT INTO user(name, password, fullname, createddate, roleid) VALUES ('traan', '123456', 'kieu thien tra an', CURRENT_TIMESTAMP, 2);
